d2df0384722dc68f32c4b376b6af60cedef76c9c
KishanSaini08 <kishan08saini@gmail.com>
2024-03-22 13:47:06 +0530

    data

---
+++ src/Datagrid.jsx
@@ -0,0 +1,83 @@
+import React, { useMemo } from 'react';
+import { useTable, useSortBy, useFilters, useGlobalFilter } from 'react-table';
+import "./App.css"
+
+
+
+const Datagrid = ({ data }) => {
+  const columns = useMemo(
+    () => [
+      { Header: 'ID', accessor: 'id' },
+      { Header: 'Customer', 
+        accessor: 'name', 
+        Cell: ({ row }) => (
+          <>
+            <img src={row.original.photo} alt="customer" style={{ width: 50 , height:50, borderRadius:50 }} />
+            {row.original.name}
+          </>
+        )
+      },
+      { Header: 'Order', accessor: 'order' },
+      { Header: 'Total Spent', accessor: 'totelspent' },
+      { Header: 'Last Seen', accessor: 'lastSeen' },
+    ],
+    []
+  );
+  
+  const { getTableProps,getTableBodyProps,headerGroups,rows, prepareRow,state,setGlobalFilter,} = useTable(
+    {
+      columns,
+      data,
+    },
+    useFilters,
+    useGlobalFilter,
+    useSortBy
+  );
+
+  const { globalFilter } = state;
+
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={globalFilter || ''}
+        onChange={(e) => setGlobalFilter(e.target.value)}
+      />
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render('Header')}
+                  <span>
+                    {column.isSorted ? (column.isSortedDesc ? ' ↓' : ' ↑') : ''}
+                  </span>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => (
+                  <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+
+export default Datagrid;
+
+
+
